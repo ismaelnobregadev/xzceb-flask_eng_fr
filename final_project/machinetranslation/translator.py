6748e58676856ec6b5061c61763379ed7d911f7d
Ismael Nóbrega <ismaelnobregadev@gmail.com>
2022-09-19 16:35:36 -0400

    updated project to save Skills Network

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,11 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['API_KEY']
+url = os.environ['API_URL']
