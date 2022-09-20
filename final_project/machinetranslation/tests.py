76f01b7db23bcc930ffdc90a7803d8beebd73666
Ismael Nóbrega <ismaelnobregadev@gmail.com>
2022-09-20 15:07:04 -0400

    updated unit test

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,36 +3,27 @@ import unittest
 from translator import englishToFrench, frenchToEnglish
 
 class TestTranslationEnglishFrench(unittest.TestCase):
-
+   
     def test_null(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertTrue(englishToFrench(''), None)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_translation(self):
+        e = 'Hello'
+        f = 'Bonjour'
+        self.assertEqual(englishToFrench(e), f)
+        
+        
 
 class TestTranslationFrenchenglish(unittest.TestCase):
 
     def test_null(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertTrue(frenchToEnglish(''), None)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_translation(self):
+        e = 'Hello'
+        f = 'Bonjour'
+        self.assertEqual(frenchToEnglish(f), e)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
+    
 if __name__ == '__main__':
     unittest.main()
