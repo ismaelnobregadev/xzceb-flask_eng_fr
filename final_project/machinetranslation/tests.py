157d7282c6bf07fce852aac8393f1839afdb27b2
Ismael Nóbrega <ismaelnobregadev@gmail.com>
2022-09-20 14:50:52 -0400

    updated unit test

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -2,9 +2,25 @@ import unittest
 
 from translator import englishToFrench, frenchToEnglish
 
-class TestTranslationEnglishFrenchMethods(unittest.TestCase):
+class TestTranslationEnglishFrench(unittest.TestCase):
 
-    def test_upper(self):
+    def test_null(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+class TestTranslationFrenchenglish(unittest.TestCase):
+
+    def test_null(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
