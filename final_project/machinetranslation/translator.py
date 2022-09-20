b5d8b1b8c7b9e95459fedfe80e7e573fc11c3552
Ismael Nóbrega <ismaelnobregadev@gmail.com>
2022-09-20 14:27:10 -0400

    updated project

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,5 +1,6 @@
 import json
 from ibm_watson import LanguageTranslatorV3
+
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 import os
@@ -9,3 +10,38 @@ load_dotenv()
 
 apikey = os.environ['API_KEY']
 url = os.environ['API_URL']
+
+authenticator = IAMAuthenticator(apikey)
+
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def englishToFrench(englishText):
+    
+    """
+    translate english text to french
+    """
+    if englishText is None:
+        return None
+    response = language_translator.translate(text=englishText,model_id="en-fr").get_result()
+
+    frenchText = response['translations'][0]['translation']
+
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    
+    """
+    translate french to english
+    """
+    if frenchText is None:
+        return None
+    response = language_translator.translate(text=frenchText,model_id="fr-en").get_result()
+    englishText = response['translations'][0]['translation']
+    
+    return englishText
+
